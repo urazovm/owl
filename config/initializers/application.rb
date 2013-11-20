ce230e533518635d515d0b1e5760377a83523248
Vincent Cogne <xpac27@gmail.com>
2013-11-20 12:48:54 +0100

    display category info on lists index

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,11 +1,11 @@
 ListForbiddenNames = ['users', 'lists']
 ListCategories = [
-  ['category_1', 0],
-  ['category_2', 1],
-  ['category_3', 2],
-  ['category_4', 3],
-  ['category_5', 4],
-  ['category_6', 5],
-  ['category_7', 6],
-  ['category_8', 7]
+  ['First category', 0],
+  ['Second category', 1],
+  ['Third category', 2],
+  ['Fourth category', 3],
+  ['Fivth category', 4],
+  ['Sixth category', 5],
+  ['Seventh category', 6],
+  ['Heigth category', 7]
 ]
