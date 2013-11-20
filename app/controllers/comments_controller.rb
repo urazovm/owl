b72a3f81e91f87e00a57701b8d7bda333238abb6
Vincent Cogne <xpac27@gmail.com>
2013-11-20 19:07:19 +0100

    change layout

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: [:create, :destroy]
 
   def create
     @list = List.find(params[:list_id])
