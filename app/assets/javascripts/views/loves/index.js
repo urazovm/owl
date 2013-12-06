cde2ef76381f00c542cf0ce5f71f37a2b24a9ab2
Vincent Cogne <xpac27@gmail.com>
2013-12-06 13:03:28 +0100

    refresh loves page when I love/ignore a list

--- app/assets/javascripts/views/loves/index.js
+++ app/assets/javascripts/views/loves/index.js
@@ -1,5 +1,11 @@
 window.owl.views.loves_index = function()
 {
+    var current_user = typeof window.owl.env['current_user'] == 'string' ? window.owl.env['current_user'] : '';
+
+    if (current_user != '') {
+        $('#user_' + current_user + ' .follow').remove();
+    }
+
     $('#user_links .lovings').addClass('active');
     $('#list .lovers').addClass('active');
 }
