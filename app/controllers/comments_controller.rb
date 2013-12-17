14a78b8c002d2705e8bc1a7e8ad425402ef3a412
Vincent Cogne <xpac27@gmail.com>
2013-12-17 15:04:04 +0100

    fix comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,21 +4,18 @@ class CommentsController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @comment = @list.comments.new({user: current_user}.merge(comments_params))
-    redirect_to list_comments_path(@list) and return if @list.save && @comment.errors.empty?
+    @comment = @list.comments.build if @list.save && @comment.errors.empty?
     @comments = @list.comments.where(:created_at.exists => true).desc(:created_at).includes(:user)
-    @category_id = @list.category_id
-    @user = @list.user
     render :index
   end
 
   def destroy
     list = List.find(params[:list_id])
     list.comments.destroy_all(id: params[:id], user_id: current_user.id)
-    redirect_to list_comments_path(list)
+    index and render :index
   end
 
   def index
-    redirect_to home_path unless request.xhr?
     list = List.find(params[:list_id])
     @comments = list.comments.where(:created_at.exists => true).desc(:created_at).includes(:user)
     @comment = list.comments.build
