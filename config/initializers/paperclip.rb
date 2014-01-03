f5d5a72334e4d3ea45e53692c047e03f0f4873cf
Vincent Cogne <xpac27@gmail.com>
2014-01-03 18:48:32 +0100

    fix typo

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,7 +2,7 @@ Paperclip::Attachment.default_options.merge!(
   storage: :fog,
   fog_attributes: { 'Cache-Control' => 'max-age=315576000' },
   fog_public: true,
-  fog_directory: ENV['S3_BUCKET']
+  fog_directory: ENV['S3_BUCKET'],
   fog_credentials: {
     provider: 'AWS',
     aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
